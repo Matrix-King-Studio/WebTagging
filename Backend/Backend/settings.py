2636d9ae560b0fc05579400d2070051bfe6daeed
Alex <alex18812649207@gmail.com>
2020-07-28 20:00:58 +0800

    修改配置数据库

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '20001001',
+        'NAME': 'webtagging',
     }
 }
 
