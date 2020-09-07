e6af4e145ed0b9c4dc41401a8728f8db505e4dda
Alex <alex18812649207@gmail.com>
2020-09-07 10:19:30 +0800

    2020-9-7

--- an/src/cvat-core/src/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    backendAPI: 'http://alexking.site:8080/api/v1',
-    proxy: false,
-};
--- an/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    devServer: {
-        open: true,
-        port: 8888
-    },
-    lintOnSave: false,
-    publicPath: '/'
-}
