abf4077c407112282c544fd87dc9518e84e0de8f
dabaishu <1250709351@qq.com>
2020-11-30 13:11:54 +0800

    '异步获取图片'

--- Frontend/src/components/allproject/project.vue
+++ Frontend/src/components/allproject/project.vue
@@ -18,50 +18,52 @@ export default {
     return {
       ifAdmin: '',
       projectData: [],
-      userId: 0,
+      page: 1,
     }
   },
-  created() {
+  mounted() {
     this.getUserInfo()
   },
-  mounted() {
-    setTimeout(() => {
-      this.getAllProject()
-    }, 200)
-  },
   methods: {
-    getAllProject() {
-      console.log('3.开始获取项目信息');
+    getAllProject(userId) {
       this.$http.get('v1/tasks', {
         params: {
-          pagesize: 9,
-          page: 1
+          pagesize: 10,
+          page: this.page
         }
       }).then((res) => {
-        console.log('4.项目信息获取完成');
-        console.log(res);
+        console.log('2.获取第' + this.page + '页项目信息', res);
+        //查找所有的项目
         for (let index = 0; index < res.data.results.length; index++) {
-          /* Alex */
+          //筛选 掉 没有分配的项目
           if (res.data.results[index].segments[0] !== undefined) {
             for (let i = 0; i < res.data.results[index].segments[0].jobs.length; i++) {
-              if (res.data.results[index].segments[0].jobs[i].assignee === this.userId || res.data.results[index].owner === this.userId) {
+              //筛选 出 是拥有者或者是被分配的项目
+              if (res.data.results[index].segments[0].jobs[i].assignee === userId || res.data.results[index].owner === userId) {
                 this.projectData.push(res.data.results[index])
               }
             }
           }
         }
-        console.log('2.项目信息渲染完成');
+        if(this.page * 10 <  res.data.count) {
+          this.page = this.page + 1
+          this.getAllProject(userId)
+        } else {
+          console.log('3.项目信息获取完成');
+        }
       })
     },
     getUserInfo() {
-      console.log('1.开始获取用户信息');
       this.$http.get('v1/users/self').then((res) => {
-        this.userId = res.data.id
         this.ifAdmin = res.data.groups.find(val => {
           return val === 'admin'
         })
-        console.log('2.获取用户信息完成');
-
+        console.log('1.获取用户信息完成, 用户id:' + res.data.id);
+        return new Promise(resolve => {
+          resolve(res.data.id)
+        })
+      }).then(value => {
+        this.getAllProject(value)
       })
     }
   },
